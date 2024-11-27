8abb8a5c3096de9a7b9058a6c186f90883cc01d5
源 <cyfang@yuandeMac-mini.ctc>
2024-11-27 23:16:02 +0800

    feat: 121

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config({
+  // tseslint.config添加了extends扁平函数，直接用。否则是eslint9.0版本是没有extends的
+  extends: [...tseslint.configs.recommended],
+  rules: {
+    'no-debugger': 'error',
+  },
+})
